b0f88fa121292e37b1d4e877b11d12f148101eee
umleigoqualquer <leozinhosouza3001@gmail.com>
2026-09-23 18:34:32 -0300

    Fix: basePath para serverless-http no Netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
 const app = require('./_lib/app');
 
-exports.handler = serverless(app);
+exports.handler = serverless(app, { basePath: '/.netlify/functions/api' });
